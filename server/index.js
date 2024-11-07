277f13fe12bb1de805d37b810d65eb613d24098d
mahendrajangid2583 <158497182+mahendrajangid2583@users.noreply.github.com>
2024-11-08 01:13:31 +0530

    Update index.js
    cors origin updated

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.use(cookieParser());
 
 app.use(
     cors({
-        origin:"http://localhost:3000",
+        origin:"https://studynotion-hosting-orcin.vercel.app",
         credentials:true
     })
 )

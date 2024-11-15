9aa073b47cc399c1637cc5fb305187bd4162f676
mahendrajangid2583 <158497182+mahendrajangid2583@users.noreply.github.com>
2024-11-15 23:30:11 +0530

    Update ProfileDropDown.jsx

--- src/components/core/Auth/ProfileDropDown.jsx
+++ src/components/core/Auth/ProfileDropDown.jsx
@@ -6,7 +6,7 @@ import { useOnClickOutside } from '../../../hooks/useOnClickOutside';
 import { CiLogout } from "react-icons/ci";
 import { RiDashboardHorizontalLine } from "react-icons/ri";
 import { CgProfile } from "react-icons/cg";
-
+import { TiArrowSortedDown } from "react-icons/ti";
 
 const ProfileDropDown = () => {
 
@@ -24,15 +24,21 @@ const ProfileDropDown = () => {
  
 
   return (
-    <div className='flex flex-col items-center relative'>
-      
-      <img className=' relative rounded-full w-8 h-8  group cursor-pointer' src={user.image}
-        onClick={()=>setShowDropDown(!showDropDown)}
-      />
+    <div className='flex flex-col items-center relative'
+      onClick={(e) => e.stopPropagation()}
+        ref={ref}
+      >
+      <div className=' flex text-richblack-50 items-center cursor-pointer'
+       onClick={()=>setShowDropDown(!showDropDown)}>
+        <img className=' relative rounded-full w-8 h-8  group ' src={user.image}
+          
+        />
+        <TiArrowSortedDown size={24}/>
+        
+      </div>
       
       <div  
-        onClick={(e) => e.stopPropagation()}
-        ref={ref}
+        
         className= {`absolute z-10 flex flex-col gap-2  top-10 right-0 w-[200px] bg-richblack-600 px-2 py-2 rounded-lg
                         ${showDropDown ? "visible": "invisible" }`}>
         {/* {

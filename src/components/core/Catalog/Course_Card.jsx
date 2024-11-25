9bf9648e8052733542c9d620abf33edb7159ac1c
mahendrajangid2583 <158497182+mahendrajangid2583@users.noreply.github.com>
2024-11-26 01:27:31 +0530

    Update Course_Card.jsx

--- src/components/core/Catalog/Course_Card.jsx
+++ src/components/core/Catalog/Course_Card.jsx
@@ -23,7 +23,7 @@ const Course_Card = ({course, Height}) => {
             <img
               src={course?.thumbnail}
               alt="course thumnail"
-              className={`${Height} w-full rounded-xl object-cover `}
+              className={`${Height}  h-[230px] lg:h-[250px] w-full rounded-xl object-cover `}
             />
           </div>
           <div className="flex flex-col gap-2 px-1 py-3">
